e62e2b167999fee26190030eb57697006ee41a07
Orishmueli <43569659+OriShmueli@users.noreply.github.com>
2023-03-23 22:16:00 +0200

    comments added

--- WindowsFormsAppGithubTest/Form1.cs
+++ WindowsFormsAppGithubTest/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsAppGithubTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //hello from winform
+        }
     }
 }
